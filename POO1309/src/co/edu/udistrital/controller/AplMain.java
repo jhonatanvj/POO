02746bfeb6fc65726e26c2f612a76f5327555a5b
jhonatanvj <143757747+jhonatanvj@users.noreply.github.com>
2023-09-14 17:24:21 -0500

    Add files via upload

--- POO1309/src/co/edu/udistrital/controller/AplMain.java
+++ POO1309/src/co/edu/udistrital/controller/AplMain.java
@@ -1,5 +1,7 @@
 package co.edu.udistrital.controller;
 
+import java.util.Scanner;
+
 import co.edu.udistrital.model.Estudiante;
 import co.edu.udistrital.model.Persona;
 import co.edu.udistrital.model.Profesor;
@@ -7,22 +9,45 @@ import co.edu.udistrital.model.Profesor;
 public class AplMain {
 
 	public static void main(String[] args) {
-	
-		Persona a=new Persona("Carlos",19);
-		
-		Estudiante b=new Estudiante("Ana",21,"Economia");
-		
-		Profesor c=new Profesor("Jhon",60,12,"Contabilidad");
-		
+		Scanner leer = new Scanner(System.in);
+
+		Persona a = new Persona("Carlos", 19);
+
+		Estudiante b = new Estudiante("Ana", 21, "Economia");
+
+		Profesor c = new Profesor("Jhon", 60, 12, "Contabilidad");
+
 		System.out.println(a);
 		System.out.println("____________________________");
 		System.out.println(b);
 		System.out.println("_________________________");
 		System.out.println(c);
 		System.out.println("_________________________");
-		
-				
-				
+
+		Persona[] d = new Persona[5];
+		int n;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Escoja 1 si quiere profesor " + "\nEscoja 2 para estudiante");
+			n = leer.nextInt();
+
+			if (n == 1) {
+				String nombre1;
+				nombre1=leer.nextLine();
+				d[i] = new Profesor(nombre1,60, 12, "Contabilidad");
+
+			} else {
+				 d[i] = new Estudiante("Ana", 21, "Economia");
+			}
+
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(d[i].toString());
+			System.out.println("______________________________");
+		}
+
+		leer.close();
 	}
 
 }
