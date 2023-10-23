edfe12b4ecc108b77a0c65c08d3f92aa8f509954
jhonatanvj <143757747+jhonatanvj@users.noreply.github.com>
2023-10-23 16:57:10 -0500

    Add files via upload

---
+++ POOGraficoPropioexception/src/co/edu/udistrital/controller/Controller.java
@@ -0,0 +1,49 @@
+package co.edu.udistrital.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import co.edu.udistrital.model.Numero;
+import co.edu.udistrital.view.VentanaPrincipal;
+
+public class Controller implements ActionListener {
+
+	private VentanaPrincipal vista;
+
+	public Controller() {
+		vista = new VentanaPrincipal();
+		asignarOyentes();
+	}
+
+	public void asignarOyentes() {
+		vista.getPanel().getBotonValidar().addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int aux2;
+
+		String comando = e.getActionCommand();
+		if (comando.equals("VALIDAR")) {
+			String aux1 = vista.getPanel().getCingresarNumero().getText();
+			try {
+				aux2 = Integer.parseInt(aux1);
+				Numero num = new Numero(aux2);
+				if (num.esPar()) {
+					vista.getPanel().getEmostrar().setText("Numero ingresado es PAR");
+				} else {
+					vista.getPanel().getEmostrar().setText("Numero ingresado es Impar");
+				}
+			} catch (NumberFormatException a) {
+				vista.getPanel().getEmostrar().setText("");
+				vista.getPanel().getCingresarNumero().setText("");
+				JOptionPane.showMessageDialog(vista,"ERROR: Numero no valido","ERROR",JOptionPane.ERROR_MESSAGE);
+			}
+
+		}
+
+	}
+
+}
---
+++ POOGraficoPropioexception/src/co/edu/udistrital/view/VentanaPrincipal.java
@@ -0,0 +1,51 @@
+package co.edu.udistrital.view;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+	private PanelPrincipal panel;
+	
+	
+	public VentanaPrincipal() {
+		//Titulo de arriba
+		setTitle("Mi primer programa");
+		//Tama�o ventana
+		setSize(800,600);
+		//Que no quede nada abierto despues de cerrar
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Color del fondo
+		getContentPane().setBackground(Color.gray);
+		//Organizacion de elementos manual
+		getContentPane().setLayout(null);
+		
+		inicializarComponentes();
+		//Hace que el usuario no pueda cambiar el tama�o de la ventana
+		setResizable(false);
+		//Hace que la ventana se abra en el centro
+		setLocationRelativeTo(null);
+		//Hace que se vea la ventana
+		setVisible(true);
+	}
+	
+	public void inicializarComponentes() {
+		
+		panel=new PanelPrincipal();
+		//Pone las coordenadas del panel osea tama�o
+		panel.setBounds(25,25,735,510);
+		//A�ade el panel
+		getContentPane().add(panel);
+	}
+
+	public PanelPrincipal getPanel() {
+		return panel;
+	}
+
+	public void setPanel(PanelPrincipal panel) {
+		this.panel = panel;
+	}
+	
+	
+}
